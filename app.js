a802f0b86c48f47f3158d88bb84e468d16b255b6
KateM3d <ekaterina8medvedeva@gmail.com>
2022-02-02 18:50:21 -0500

    set up controllers and routes

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
+const tasks = require("./routes/tasks");
+
+//middleware
+
+app.use(express.json());
 
 //routes
-
 app.get("/hello", (req, res) => {
     res.send("task manager app");
 });
+
+ap.use("/api/v1/tasks", tasks);
 
 const port = 3000;
 
